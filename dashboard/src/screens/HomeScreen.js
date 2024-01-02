d4a5475bb492c2ba9d0f7609cd3203a7400c075d
MailBIT Sales <mailbitsales@MailBITs-MacBook-Pro.local>
2024-01-02 21:58:48 +0200

    create dashboard

---
+++ dashboard/src/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+const HomeScreen = () => {
+    return (
+        <>
+            <Sidebar />
+            <main className='main-wrap'>
+                Header
+                Main
+            </main>
+        </>
+    )
+}
+
+export default HomeScreen
